9888bddbcba49c7f54b3f2e636ba96353023f6b4
zkfy <lib4net@163.com>
2019-10-21 10:56:27 +0800

    稍加认证服务

--- rpcserver/main.go
+++ rpcserver/main.go
@@ -1,33 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"time"
-
-	"github.com/micro-plat/hydra/registry"
 	_ "github.com/micro-plat/hydra/registry/zookeeper"
-	"github.com/micro-plat/lib4go/logger"
 )
 
 func main() {
-	///single/apiserver/md5/auth@authserver.
-	start := time.Now()
-	var err error
-	watch := &Watcher{plat: "sas_debug", server: "authserver", service: "/single/apiserver/sha1/auth"}
-	watch.client, err = registry.NewRegistryWithAddress("zk://192.168.0.101", logger.New("t"))
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(watch.initialize())
-	fmt.Println(time.Since(start))
 
-}
-
-type Watcher struct {
-	client  registry.IRegistry
-	plat    string
-	server  string
-	path    string
-	service string
 }
