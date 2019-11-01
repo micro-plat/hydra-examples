56ab9c2f1d2c25769b239a865ad931b50bf3dfd7
zkfy <lib4net@163.com>
2019-11-01 16:48:38 +0800

    增加测试用例

--- apiserver01/main.go
+++ apiserver01/main.go
@@ -1,3 +1,10 @@
+/*
+最简单的apiserver示例
+1. 将函数注册为服务
+2. 服务支持任意类型数据结构作为返回值
+3. 从路由中获取参数的方法
+*/
+
 package main
 
 import "github.com/micro-plat/hydra/hydra"
@@ -10,7 +17,6 @@ var app = hydra.NewApp(
 	hydra.WithDebug())
 
 func main() {
-	app.Micro("/order/request", request)
 	app.Micro("/order/request/:tp", request)
 	app.Start()
 }
