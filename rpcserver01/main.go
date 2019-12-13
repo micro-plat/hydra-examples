87f0dae62234eaa28eccf3459568c3400f9cad80
zkfy <lib4net@163.com>
2019-12-13 11:11:19 +0800

    添加rpcserver

---
+++ rpcserver01/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/micro-plat/hydra/context"
+
+	"github.com/micro-plat/hydra/component"
+	"github.com/micro-plat/hydra/hydra"
+)
+
+var app = hydra.NewApp(
+	hydra.WithPlatName("hydra-examples"),
+	hydra.WithSystemName("rpcserver01"),
+	hydra.WithServerTypes("api-rpc"),
+	hydra.WithClusterName("test"))
+
+func main() {
+	app.Micro("/order/query", query)
+	app.Micro("/order/request", component.NewRPCSerivce("/order/query@rpcserver01.hydra-examples", map[string]string{
+		"sysid": "abc",
+	}))
+	app.Start()
+}
+func query(ctx *context.Context) (r interface{}) {
+	ctx.Log.Info("------query------")
+	return ctx.Request.GetString("sysid")
+}
